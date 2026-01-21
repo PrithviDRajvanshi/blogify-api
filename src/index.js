e438690a614651ae475b8614c5d4966941212205
Prithvi <prithvidrajvanshi@gmail.com>
2026-01-21 15:54:32 +0530

    Refactor:extract postroutes to modular router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const http = require("http");
-
+const express = require("express");
+const app = express();
 const PORT = 3000;
+const postsRouter = require("./routes/posts.routes");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Blogify API is running!");
+app.get("/", (req, res) => {
+  res.send("Blogify API is running!");
 });
 
-server.listen(PORT, () => {
+app.use("/api/v1/posts", postsRouter);
+
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
