3b783becfd4d4c96be9750334be06b8bc2357c19
Prithvi <prithvidrajvanshi@gmail.com>
2026-02-12 15:45:42 +0530

    Finalizing Architecture for Module 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-const express = require("express");
-const app = express();
+require('dotenv').config();
 
-const postsRouter = require("./routes/posts.routes");
-const usersRouter = require("./routes/user.routes");
+const app = require('./app');
 
-app.get("/", (req, res) => {
-  res.send("Blogify API is running!");
-});
+const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/posts", postsRouter);
-app.use("/api/v1/users", usersRouter);
-
-const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
